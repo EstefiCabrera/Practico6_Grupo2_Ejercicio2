4f9ddf0029edddbc67b9c5a5756882c6c4990430
Estefania Cabrera <estefi.cabrera22@gmail.com>
2026-09-23 13:06:51 -0300

    Initial commit

---
+++ src/practico6_grupo2_ejercicio2/Practico6_Grupo2_Ejercicio2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practico6_grupo2_ejercicio2;
+
+/**
+ *
+ * @author estef
+ */
+public class Practico6_Grupo2_Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
